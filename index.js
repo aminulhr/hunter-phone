ff0bf0cd04063edab51e76a4e76a75047520d996
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-23 12:41:36 +0600

    load phones from API

---
+++ index.js
@@ -0,0 +1,15 @@
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displyPhone(phones);
+};
+const displyPhone = (phones) => {
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
+};
+
+loadPhone();
