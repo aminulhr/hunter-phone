ef3fe0b4d589a15560617807e808f451ea5d067b
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-24 18:44:05 +0600

    add searh bar

--- index.js
+++ index.js
@@ -7,9 +7,36 @@ const loadPhone = async () => {
   displyPhone(phones);
 };
 const displyPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
   phones.forEach((phone) => {
     console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-300 w-96 shadow-xl p-4 rounded-xl`;
+    phoneCard.innerHTML = `
+                    <figure Class="flex justify-center">
+              <img Class='rounded-md '
+                src="${phone.image}"
+                alt="Shoes"/> </figure>
+           
+              
+
+            <div class="card-body ">
+              <h2 class="card-title flex justify-center">${phone.phone_name}</h2>
+              <p Class="flex justify-center">If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions flex justify-center">
+                <button class="btn btn-primary ">Buy Now</button>
+              </div>
+            </div>
+          </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
   });
 };
 
 loadPhone();
+// handel search
+const handelSeardh = () => {
+  const getSearch = document.getElementById("search-filde");
+  const getSearchValue = getSearch.value;
+  console.log(getSearchValue);
+};
